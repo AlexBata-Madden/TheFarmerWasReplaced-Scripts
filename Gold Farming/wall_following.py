b8c2e4ca14c0856156a56b83906602639680b65f
Alex <alexbatamadden@gmail.com>
2026-06-05 18:53:41 +0100

    put wall_following code into a method

--- Gold Farming/wall_following.py
+++ Gold Farming/wall_following.py
@@ -1,21 +1,24 @@
 def generate_full_map_maze():
-	clear()
 	plant(Entities.Bush)
 	use_item(Items.Weird_Substance, get_world_size() * 2**(num_unlocked(Unlocks.Mazes) - 1))
-
-generate_full_map_maze()
-
-directions = [West, North, East, South]
-
-while True:
-	heading = 0
 	
-	while get_entity_type() != Entities.Treasure:
-		
-		heading = (heading - 1) % 4
-		
-		while not move(directions[heading]):
-			heading = (heading + 1) % 4
-		
-	harvest()
+def farm_gold():
+	directions = [West, North, East, South]
+	
+	clear()
 	generate_full_map_maze()
+
+	while True:
+		heading = 0
+		
+		while get_entity_type() != Entities.Treasure:
+			
+			heading = (heading - 1) % 4
+			
+			while not move(directions[heading]):
+				heading = (heading + 1) % 4
+			
+		harvest()
+		generate_full_map_maze()
+
+farm_gold()
